85a79af58d187f85c89a59535133fd230d5775ec
taboubi <taboubioussema78@gmail.com>
2024-03-15 14:54:36 +0100

    first commit

--- Scroll.js
+++ Scroll.js
@@ -7,3 +7,9 @@ function scrollToskills() {
 function scrollToAboutMe() {
   document.getElementById("aboutMe").scrollIntoView({ behavior: "smooth" });
 }
+function scrollToCertificate() {
+  document.getElementById("certificate").scrollIntoView({ behavior: "smooth" });
+}
+function scrollToContactMe() {
+  document.getElementById("contactMe").scrollIntoView({ behavior: "smooth" });
+}
